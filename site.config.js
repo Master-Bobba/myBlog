f4dbfc674525371e836f3049a555d34f96e813e7
Bobby Ayvazov <99765912+Master-Bobba@users.noreply.github.com>
2023-12-13 14:20:18 +0000

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,31 +1,31 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
-    image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
-    instagram: "",
+    name: "Bozhidar Ayvazov",
+    image: "", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app -> "/avatar.svg"
+    role: "Software Engineer",
+    bio: "I like writing code..",
+    email: "bgayvazov@gmail.com",
+    linkedin: "bozhidar-ayvazov-362469a0n",
+    github: "Master-Bobba",
+    instagram: "bayvazov",
   },
   projects: [
     {
-      name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
+      name: `Master-Bobba`,
+      href: "https://github.com/Master-Bobba/myBlog",
     },
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "Master-Bobba's Blog",
+    description: "welcome to my blog!",
   },
 
   // CONFIG configration (required)
   link: "https://morethan-log.vercel.app",
-  since: 2022, // If leave this empty, current year will be used.
-  lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
+  since:, // If leave this empty, current year will be used.
+  lang: "en-UK", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
 
   // notion configuration (required)
